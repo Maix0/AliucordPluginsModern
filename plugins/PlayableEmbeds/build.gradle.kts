35c9889c8d8a9649c4122b9ade686bff1932f1ad
Maix0 <39835848+Maix0@users.noreply.github.com>
2026-05-24 01:43:07 +0200

    feat(PlayableEmbeds): Fixed youtube embeds

---
+++ plugins/PlayableEmbeds/build.gradle.kts
@@ -0,0 +1,9 @@
+version = "0.0.0"
+description = "Makes Spotify and Youtube Embeds playable - HOMEMADE FORK"
+
+aliucord { 
+    changelog.set("""
+        # 1.0.2
+        * Youtube embed now supports timestamps
+    """.trimIndent())
+}
