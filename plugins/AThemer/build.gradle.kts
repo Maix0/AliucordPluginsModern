d20182c08f6883e6a812b113e8eb21658ea2b160
Maix0 <39835848+Maix0@users.noreply.github.com>
2026-05-23 23:15:12 +0200

    feat(Themer): added Themer from vindicated's repository

---
+++ plugins/AThemer/build.gradle.kts
@@ -0,0 +1,22 @@
+version = "0.0.0"
+description = "Apply custom themes to your Discord - THIS IS A HOMEMADE FORK DO NOT USE"
+
+aliucord { 
+    changelog.set(
+        """
+        # 3.6.4
+        * Fix colour theming for some elements like user profile header and chatbox
+
+        # 3.6.3
+        * Fix colour theming for newer android versions
+
+        # 3.6.2
+        * Now prompts to switch to dark mode if using light/pureEvil theme
+        
+        # 3.6.1
+        * Re-enabled custom fonts. They may still be unstable, so use at your own risk  
+    """.trimIndent()
+    )
+
+    deploy.set(false)
+}
